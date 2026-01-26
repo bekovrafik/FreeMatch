a1a211482c54f8c21f03fe70b5b57956403acd91
bekovrafik <bekovrafik@gmail.com>
2026-01-26 23:52:42 +0400

    Refactor package name and update styles

--- android/app/src/main/kotlin/com/freematch/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/freematch/app/MainActivity.kt
@@ -2,4 +2,12 @@ package com.freematch.app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+import androidx.core.view.WindowCompat
+
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: android.os.Bundle?) {
+        // Aligns the Window with the screen edges to allow edge-to-edge content.
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+    }
+}
--- android/app/src/main/kotlin/com/pilllens/freematch/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.pilllens.freematch
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
